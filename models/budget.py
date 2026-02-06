27aecb4d7724c94269ce538bf65b506b545e32e3
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-05 23:54:59 -0500

    Add to_dict()

--- models/budget.py
+++ models/budget.py
@@ -2,3 +2,9 @@ class Budget:
     def __init__(self, category, limit):
         self.category = category
         self.limit = limit
+
+    def to_dict(self):
+        return {
+            "category": self.category,
+            "limit": self.limit
+        }
