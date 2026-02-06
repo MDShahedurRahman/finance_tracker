9bf0244aad04fe975ce17aebe9b3ba2c6506223c
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-06 00:02:50 -0500

    Implement Menu

---
+++ views/finance_view.py
@@ -0,0 +1,9 @@
+class FinanceView:
+
+    def show_menu(self):
+        print("\n=== Finance Tracker Pro ===")
+        print("1. Add Transaction")
+        print("2. View Transactions")
+        print("3. Delete Transaction")
+        print("4. Monthly Summary")
+        print("0. Exit")
