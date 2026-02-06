21184c381cd44e031288ee981ba1df8402a9985c
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-05 23:58:27 -0500

    Add load_all() method

--- repositories/budget_repository.py
+++ repositories/budget_repository.py
@@ -6,3 +6,11 @@ from models.budget import Budget
 class BudgetRepository:
     def __init__(self, file_path="data/budgets.json"):
         self.file_path = file_path
+
+    def load_all(self):
+        if not os.path.exists(self.file_path):
+            return []
+
+        with open(self.file_path, "r") as f:
+            data = json.load(f)
+            return [Budget.from_dict(x) for x in data]
