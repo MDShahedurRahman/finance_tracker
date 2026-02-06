d5ff0f9750c2bc5e7e47aca1d04cd0e52dd4c858
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-05 23:53:25 -0500

    Add to_dict() method

--- models/transaction.py
+++ models/transaction.py
@@ -8,3 +8,12 @@ class Transaction:
         self.category = category
         self.amount = amount
         self.date = date
+
+    def to_dict(self):
+        return {
+            "txn_id": self.txn_id,
+            "txn_type": self.txn_type,
+            "category": self.category,
+            "amount": self.amount,
+            "date": self.date
+        }
