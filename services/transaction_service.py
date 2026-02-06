75a07c44dd91e1cbb3c4d0fe80fbb64dcab1f959
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-05 23:59:50 -0500

    Add transaction

--- services/transaction_service.py
+++ services/transaction_service.py
@@ -4,3 +4,8 @@ from repositories.transaction_repository import TransactionRepository
 class TransactionService:
     def __init__(self):
         self.repo = TransactionRepository()
+
+    def add_transaction(self, txn):
+        txns = self.repo.load_all()
+        txns.append(txn)
+        self.repo.save_all(txns)
