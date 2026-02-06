458954fa7c7b2b4c7631f302c6617a60a1c92f7f
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-05 23:56:54 -0500

    Add load_all() method

--- repositories/transaction_repository.py
+++ repositories/transaction_repository.py
@@ -6,3 +6,11 @@ from models.transaction import Transaction
 class TransactionRepository:
     def __init__(self, file_path="data/transactions.json"):
         self.file_path = file_path
+
+    def load_all(self):
+        if not os.path.exists(self.file_path):
+            return []
+
+        with open(self.file_path, "r") as f:
+            data = json.load(f)
+            return [Transaction.from_dict(x) for x in data]
