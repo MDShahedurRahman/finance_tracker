943bef75cf0ee38422b208ad84036af13ff5f56e
Md Shahedur Rahman <mr6139@nyu.edu>
2026-02-06 21:56:14 -0500

    implement class FinanceController

---
+++ controllers/finance_controller.py
@@ -0,0 +1,7 @@
+from models.transaction import Transaction
+from services.transaction_service import TransactionService
+from services.analytics_service import AnalyticsService
+from views.finance_view import FinanceView
+
+
+class FinanceController:
